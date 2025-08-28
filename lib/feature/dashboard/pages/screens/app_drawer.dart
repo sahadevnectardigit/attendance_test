f3a4e7150aa30c80696cea95b9b6e8ad9bcec66d
sahadev <sahadev.kunwar@nectardigit.com>
2025-08-28 18:09:46 +0545

    logout added

--- lib/feature/dashboard/pages/screens/app_drawer.dart
+++ lib/feature/dashboard/pages/screens/app_drawer.dart
@@ -1,3 +1,5 @@
+import 'package:attendance/core/services/local_storage.dart';
+import 'package:attendance/feature/auth/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 class CustomDrawer extends StatefulWidget {
@@ -11,82 +13,82 @@ class _CustomDrawerState extends State<CustomDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Row(
-        children: [
-          Container(
-            width: 250,
-            color: Color(0xFF4A6CF7),
-            child: Column(
-              children: [
-                SizedBox(height: 20),
-                ListTile(
-                  leading: Icon(Icons.home, color: Colors.white),
-                  title: Text('Home', style: TextStyle(color: Colors.white)),
-                  onTap: () {
-                    print('Home tapped');
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.notifications, color: Colors.white),
-                  title: Text(
-                    'Notifications',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    print('Notifications tapped');
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.message, color: Colors.white),
-                  title: Text(
-                    'Messages',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    print('Messages tapped');
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.visibility, color: Colors.white),
-                  title: Text(
-                    'Overview',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    print('Overview tapped');
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.report_problem, color: Colors.white),
-                  title: Text(
-                    'Action - Leave / Concern',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    print('Action tapped');
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.person, color: Colors.white),
-                  title: Text(
-                    'Client Visit',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    print('Client Visit tapped');
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.logout, color: Colors.white),
-                  title: Text('Logout', style: TextStyle(color: Colors.white)),
-                  onTap: () {
-                    print('Logout tapped');
-                  },
-                ),
-              ],
+      child: Container(
+        width: 250,
+        color: Color(0xFF4A6CF7),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            ListTile(
+              leading: Icon(Icons.home, color: Colors.white),
+              title: Text('Home', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                print('Home tapped');
+              },
             ),
-          ),
-        ],
+            ListTile(
+              leading: Icon(Icons.notifications, color: Colors.white),
+              title: Text(
+                'Notifications',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () {
+                print('Notifications tapped');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.message, color: Colors.white),
+              title: Text('Messages', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                print('Messages tapped');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.visibility, color: Colors.white),
+              title: Text('Overview', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                print('Overview tapped');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.report_problem, color: Colors.white),
+              title: Text(
+                'Action - Leave / Concern',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () {
+                print('Action tapped');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person, color: Colors.white),
+              title: Text(
+                'Client Visit',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () {
+                print('Client Visit tapped');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout, color: Colors.white),
+              title: Text('Logout', style: TextStyle(color: Colors.white)),
+              onTap: () async {
+                // Clear login state
+                await LocalStorage.clearTokens();
+                await LocalStorage.setRememberMe(false);
+
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AttendanceLoginPage(),
+                  ),
+                  (Route<dynamic> route) => false,
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
